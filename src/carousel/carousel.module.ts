874774d66b76e70dbe0b93f87d8a2f92f752ab7a
Galim Kaudinov <galimk@gmail.com>
2017-08-12 00:06:54 -0600

    added new component to the declarations.

--- src/carousel/carousel.module.ts
+++ src/carousel/carousel.module.ts
@@ -3,12 +3,13 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 
 import { CarouselComponent } from './carousel.component';
 import { SlideComponent } from './slide.component';
+import { ArrowComponent } from './arrow.component';
 import { CarouselConfig } from './carousel.config';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SlideComponent, CarouselComponent],
-  exports: [SlideComponent, CarouselComponent],
+  declarations: [SlideComponent, CarouselComponent, ArrowComponent],
+  exports: [SlideComponent, CarouselComponent, ArrowComponent],
   providers: [CarouselConfig]
 })
 export class CarouselModule {
--- src/carousel/index.ts
+++ src/carousel/index.ts
@@ -3,3 +3,4 @@ export { CarouselModule } from './carousel.module';
 export { SlideComponent } from './slide.component';
 export { CarouselConfig } from './carousel.config';
 export { ArrowComponent } from './arrow.component';
+export { ArrowDirection } from './arrow.component'
