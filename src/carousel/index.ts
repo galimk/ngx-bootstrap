7b78ab235f0812ab97c835e4f50cdb8669e18565
Galim Kaudinov <galimk@gmail.com>
2017-08-11 23:37:17 -0600

    Added bool flag to show hide indicator. Crated an arrow component so that default arrows can be customized

--- src/carousel/index.ts
+++ src/carousel/index.ts
@@ -2,3 +2,4 @@ export { CarouselComponent } from './carousel.component';
 export { CarouselModule } from './carousel.module';
 export { SlideComponent } from './slide.component';
 export { CarouselConfig } from './carousel.config';
+export { ArrowComponent } from './arrow.component';
